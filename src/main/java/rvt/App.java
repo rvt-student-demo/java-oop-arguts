28c475b43307dc121b1aaa4def609ac3efc734e3
Arguts Puzuls <a242211ap@rvt.lv>
2026-05-20 14:34:50 +0300

    commiting changes in todolist db

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,9 +1,27 @@
 package rvt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
     public static void main(String[] args) {
-        TodoList list = new TodoList();
-        UserInterface ui = new UserInterface(list);
-        ui.start();
+
+        try (
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:sqlite:todo.db"
+                );
+
+                Statement statement = connection.createStatement();
+        ) {
+            String sql = "CREATE TABLE IF NOT EXISTS todo"
+                    + "(id INTEGER PRIMARY KEY, task TEXT NOT NULL) STRICT";
+
+            statement.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
